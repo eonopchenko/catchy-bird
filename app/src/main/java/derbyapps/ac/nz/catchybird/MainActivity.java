2e3ea2cdf49f1f22dac17adbabf90e321933b4f9
eonopchenko <jake.spb@gmail.com>
2017-08-13 00:25:17 +1200

    +Added bird list

--- app/src/main/java/derbyapps/ac/nz/catchybird/MainActivity.java
+++ app/src/main/java/derbyapps/ac/nz/catchybird/MainActivity.java
@@ -22,29 +22,5 @@ public class MainActivity extends AppCompatActivity {
         BirdLocationClient birdLocationClient = new BirdLocationClient(this);
         birdLocationClient.setOnBirdLocationListener(
                 (BirdLocationListener)getFragmentManager().findFragmentById(R.id.first_fragment));
-
-        List<String> your_array_list = Arrays.asList(
-                "Bird 1",
-                "Bird 2",
-                "Bird 3",
-                "Bird 4",
-                "Bird 5",
-                "Bird 6",
-                "Bird 7",
-                "Bird 8",
-                "Bird 9",
-                "Bird 10"
-        );
-
-        // This is the array adapter, it takes the context of the activity as a
-        // first parameter, the type of list view as a second parameter and your
-        // array as a third parameter.
-        ArrayAdapter<String> arrayAdapter = new ArrayAdapter<String>(
-                this,
-                android.R.layout.simple_list_item_1,
-                your_array_list );
-
-        ListView lv = (ListView) findViewById(R.id.bird_list);
-        lv.setAdapter(arrayAdapter);
     }
 }
--- app/src/main/java/derbyapps/ac/nz/catchybird/MapFragment.java
+++ app/src/main/java/derbyapps/ac/nz/catchybird/MapFragment.java
@@ -5,6 +5,8 @@ import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
 
 import com.google.android.gms.maps.CameraUpdateFactory;
 import com.google.android.gms.maps.GoogleMap;
@@ -15,6 +17,7 @@ import com.google.android.gms.maps.model.CameraPosition;
 import com.google.android.gms.maps.model.LatLng;
 import com.google.android.gms.maps.model.MarkerOptions;
 
+import java.util.ArrayList;
 import java.util.List;
 
 /**
@@ -28,13 +31,31 @@ public class MapFragment extends Fragment implements OnMapReadyCallback, BirdLoc
     View mView;
 
     public void onBirdLocationAvailable(List<BirdLocationItem> locations) {
+
+        List<String> your_array_list = new ArrayList<String>();
+
+        // This is the array adapter, it takes the context of the activity as a
+        // first parameter, the type of list view as a second parameter and your
+        // array as a third parameter.
+        ArrayAdapter<String> arrayAdapter = new ArrayAdapter<String>(
+                mView.getContext(),
+                android.R.layout.simple_list_item_1,
+                your_array_list );
+
         for(BirdLocationItem location: locations) {
             float lat = location.getLatitude();
             float lng = location.getLongitude();
+            String bird = location.getBird();
             mGoogleMap.addMarker(new MarkerOptions().position(new LatLng(lat, lng)).title(location.getBird()).snippet("Such a nice bird!"));
             CameraPosition camPos = CameraPosition.builder().target(new LatLng(lat, lng)).zoom(16).bearing(0).tilt(45).build();
             mGoogleMap.moveCamera(CameraUpdateFactory.newCameraPosition(camPos));
+            if(!your_array_list.contains(bird)) {
+                your_array_list.add(bird);
+            }
         }
+
+        ListView lv = (ListView) mView.findViewById(R.id.bird_list);
+        lv.setAdapter(arrayAdapter);
     }
 
     public MapFragment() {
