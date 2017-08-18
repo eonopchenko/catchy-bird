ed6f5e385a537a5649659f25654ff19251fec7f0
eonopchenko <jake.spb@gmail.com>
2017-08-18 12:21:11 +1200

    -Deleted unneeded declaration ~Updated build tools version

--- app/src/main/java/derbyapps/ac/nz/catchybird/MainActivity.java
+++ app/src/main/java/derbyapps/ac/nz/catchybird/MainActivity.java
@@ -9,7 +9,6 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        MainActivity activity = this;
 
         /// Create BirdLocationClient
         BirdLocationClient birdLocationClient = new BirdLocationClient(this);
